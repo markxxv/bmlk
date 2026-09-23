430cb65f8ec09f479a058b57ec12e03d67d62293
markxxv <mbogrov@gmail.com>
2026-09-23 19:44:35 +0200

    Initialize cart globally

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,6 +47,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;700&family=Playfair+Display:ital,wght@0,500;0,600;1,500&display=swap" rel="stylesheet">
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    @include('layouts.cart-store')
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
     <style>
@@ -58,6 +59,7 @@
 
 <body class="min-h-screen bg-[#FCF8F4] font-['Hanken_Grotesk'] text-[#3A2E30] antialiased">
     @include('layouts.header')
+    <x-cart />
 
     {{ $slot }}
 
