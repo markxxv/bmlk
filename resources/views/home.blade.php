b448cd860d541353efc08027baf4091c6cebcc6b
markxxv <mbogrov@gmail.com>
2026-09-24 01:30:01 +0200

    Animate coffret product cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -97,7 +97,7 @@
                 </p>
             </div>
 
-            <div class="-mx-3 mt-12 flex snap-x snap-mandatory gap-2 overflow-x-auto px-5 pb-4 sm:-mx-5 sm:gap-5 sm:px-5 lg:mx-0 lg:grid lg:grid-cols-3 lg:gap-8 lg:overflow-visible lg:px-0 lg:pb-0">
+            <div class="-mx-3 mt-12 flex snap-x snap-mandatory gap-2 overflow-x-auto px-5 pb-4 sm:-mx-5 sm:gap-5 sm:px-5 lg:mx-0 lg:grid lg:grid-cols-3 lg:gap-8 lg:overflow-visible lg:px-0 lg:pb-0" data-blur-reveal data-blur-stagger="0.12" data-blur-duration="0.85" data-blur-y="22" data-blur-pixels="10">
                 @foreach ($coffrets as $product)
                     @php
                         $image = $product->getFirstMediaUrl('images');
@@ -111,7 +111,7 @@
                             : route('shop.index');
                     @endphp
 
-                    <article class="group w-4/5 shrink-0 snap-start sm:w-1/2 lg:w-auto @if ($loop->iteration === 2) lg:pt-12 @endif">
+                    <article class="group w-4/5 shrink-0 snap-start sm:w-1/2 lg:w-auto @if ($loop->iteration === 2) lg:pt-12 @endif" data-blur-reveal-item>
                         <a href="{{ $coffretUrl }}" class="block">
                             <div class="relative aspect-[3/4] overflow-hidden rounded-3xl">
                                     @if ($product->is_new)
