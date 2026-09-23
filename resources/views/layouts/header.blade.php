fc1022d3e1237a0803cea5a7c717c3095efa043e
markxxv <mbogrov@gmail.com>
2026-09-23 14:56:27 +0200

    Link where to buy navigation

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -41,7 +41,7 @@
                 </a>
 
                 <div class="hidden items-center justify-end gap-8 lg:flex">
-                    <a href="#" class="group relative py-7 text-xs font-semibold uppercase tracking-widest text-zinc-800">
+                    <a href="{{ route('where-to-buy') }}" class="group relative py-7 text-xs font-semibold uppercase tracking-widest text-zinc-800">
                         {{ __('Où acheter') }}
                         <span class="absolute inset-x-0 bottom-5 h-px origin-left scale-x-0 bg-[#A9636F] transition-transform duration-300 group-hover:scale-x-100"></span>
                     </a>
@@ -143,7 +143,7 @@
                 <a href="#" @click="open = false" class="border-b border-[#C98792] py-4 font-['Playfair_Display'] text-4xl text-zinc-900">
                     {{ __('Cours') }}
                 </a>
-                <a href="#" @click="open = false" class="border-b border-[#C98792] py-4 font-['Playfair_Display'] text-4xl text-zinc-900">
+                <a href="{{ route('where-to-buy') }}" @click="open = false" class="border-b border-[#C98792] py-4 font-['Playfair_Display'] text-4xl text-zinc-900">
                     {{ __('Où acheter') }}
                 </a>
                 <a href="#" @click="open = false" class="py-4 font-['Playfair_Display'] text-4xl text-zinc-900">
