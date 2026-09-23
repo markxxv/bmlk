7bad571483406d278ba26f379edafa8b1bca0898
markxxv <mbogrov@gmail.com>
2026-09-23 22:26:34 +0200

    Replace Tailwind and Alpine CDN with Vite bundle

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,15 +47,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@400;500;600;700&family=Playfair+Display:ital,wght@0,500;0,600;1,500&display=swap" rel="stylesheet">
 
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    @include('layouts.cart-store')
-    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 
-    <style>
-        [x-cloak] {
-            display: none !important;
-        }
-    </style>
+    @vite('resources/js/app.js')
 </head>
 
 <body class="min-h-screen bg-[#FCF8F4] font-['Hanken_Grotesk'] text-[#3A2E30] antialiased">
