2ae339dbf599625db7af249cac1d30b964d473c4
markxxv <mbogrov@gmail.com>
2026-09-23 17:31:56 +0200

    Simplify event model

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -10,33 +10,18 @@ class Event extends Model
 {
     use HasFactory, HasTranslations;
 
-    public const SALES_INTERNAL = 'internal';
-
-    public const SALES_EXTERNAL = 'external';
-
     protected $guarded = ['id'];
 
     public array $translatable = [
         'name',
         'url',
-        'type',
-        'status',
         'description',
-        'cta',
-        'date_label',
-        'country',
-        'city',
-        'venue',
-        'address',
-        'meta_title',
-        'meta_description',
     ];
 
     protected function casts(): array
     {
         return [
             'active' => 'boolean',
-            'confirmed' => 'boolean',
             'sort' => 'integer',
             'starts_at' => 'datetime',
             'ends_at' => 'datetime',
