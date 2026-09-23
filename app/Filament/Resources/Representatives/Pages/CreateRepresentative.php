5a6df9ce3ca7a0eefc7365a5d741e0184510939c
markxxv <mbogrov@gmail.com>
2026-09-23 14:52:42 +0200

    Add representative create page

---
+++ app/Filament/Resources/Representatives/Pages/CreateRepresentative.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filament\Resources\Representatives\Pages;
+
+use App\Filament\Resources\Representatives\RepresentativeResource;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRepresentative extends CreateRecord
+{
+    protected static string $resource = RepresentativeResource::class;
+}
