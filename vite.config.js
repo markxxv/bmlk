483ab828c1d5c5c5861cc46339fb559ccfd168fe
markxxv <mbogrov@gmail.com>
2026-09-23 22:26:28 +0200

    Use app.js as main frontend entry

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js', 'resources/js/product.js', 'resources/js/mascot.js'],
+            input: ['resources/js/app.js', 'resources/js/product.js', 'resources/js/mascot.js'],
             refresh: true,
             fonts: [
                 bunny('Instrument Sans', {
