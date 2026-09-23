32d789da7153f88d4458c78ccf812afae23f665d
markxxv <mbogrov@gmail.com>
2026-09-23 02:23:44 +0200

    Add padding to the circular container in home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -52,7 +52,7 @@
                 </div>
 
                 <div class="flex items-center justify-center px-5 pb-8 sm:px-10 lg:px-8 lg:py-10 xl:px-14">
-                    <div class="aspect-square w-full max-w-2xl overflow-hidden rounded-full bg-[#FCF8F4]">
+                    <div class="aspect-square w-full max-w-2xl overflow-hidden rounded-full bg-[#FCF8F4] lg:p-24">
                         <x-cow />
                     </div>
                 </div>
