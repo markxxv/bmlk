f0929399d5536ee81b28e35b0be73dbd0039d4b5
markxxv <mbogrov@gmail.com>
2026-09-23 22:26:32 +0200

    Bootstrap Tailwind and Alpine from app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,9 @@
-//
+import '../css/app.css';
+import Alpine from 'alpinejs';
+import { registerCartStore } from './stores/cart';
+
+window.Alpine = Alpine;
+
+registerCartStore(Alpine);
+
+Alpine.start();
