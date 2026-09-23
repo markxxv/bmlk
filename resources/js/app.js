446de59e93831fec8133460b60681644f7d18e67
markxxv <mbogrov@gmail.com>
2026-09-24 01:16:23 +0200

    Install Motion and initialize shared animations

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,9 +1,12 @@
 import '../css/app.css';
 import Alpine from 'alpinejs';
 import { registerCartStore } from './stores/cart';
+import { initAnimations } from './animations';
 
 window.Alpine = Alpine;
 
 registerCartStore(Alpine);
 
 Alpine.start();
+
+initAnimations();
