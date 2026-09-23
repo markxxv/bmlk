71931ca6c2ff2fadba6c18d70226d6ae53682fe9
markxxv <mbogrov@gmail.com>
2026-09-23 13:56:41 +0200

    Add shop category routes

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,9 @@ use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontController::class, 'home'])->name('home');
+
+Route::get('/shop', [FrontController::class, 'shop'])->name('shop.index');
+
+Route::get('/shop/category/{slug}', [FrontController::class, 'shopCategory'])
+    ->where('slug', '[A-Za-z0-9-]+')
+    ->name('shop.category');
