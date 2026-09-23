00f365eb9d50efae8a937786ec80e708096baef1
markxxv <mbogrov@gmail.com>
2026-09-23 20:03:32 +0200

    Add CSRF token for checkout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ $title ?? __('BLACK MILK') }}</title>
     <meta name="robots" content="{{ $robots }}">
